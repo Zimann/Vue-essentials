2c7d3ee9e889ea5eef213571474f07cc719cdf36
nicolaeziman <20334064+Zimann@users.noreply.github.com>
2022-02-28 18:46:26 +0200

    create initial setup

---
+++ video-browser/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <SearchBar @termChange="onTermChange">
+    </SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from "@/components/SearchBar";
+const API_KEY = 'AIzaSyC9pBPv7a8mMr2hj-Zu0goiFOQbB3nvQ0A';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      searchData: 'search data',
+    }
+  },
+  components: {
+    SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      console.log(searchTerm);
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ video-browser/src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input @input="onInput" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    text: String,
+  },
+  methods: {
+    onInput(event){
+      this.$emit('termChange', event.target.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
